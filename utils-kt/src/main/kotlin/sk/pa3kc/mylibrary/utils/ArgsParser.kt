f10996b3d2aac331313c5f1021c5f8c47ea6737b
Pa3kc123 <pa3kc123@gmail.com>
2020-11-17 22:33:08 +0100

    Update
    Moved kotlin operator functions for util module to top level

--- utils-kt/src/main/kotlin/sk/pa3kc/mylibrary/utils/ArgsParser.kt
+++ utils-kt/src/main/kotlin/sk/pa3kc/mylibrary/utils/ArgsParser.kt
@@ -1,7 +1,5 @@
 package sk.pa3kc.mylibrary.utils
 
-object ArgsParserKt {
-    operator fun ArgsParser.get(i: Int): String = this.args[i]
-    operator fun ArgsParser.get(key: String): String? = this.options[key]
-    operator fun ArgsParser.get(key: String, def: String): String = this.options[key] ?: def
-}
+operator fun ArgsParser.get(i: Int): String = this.args[i]
+operator fun ArgsParser.get(key: String): String? = this.options[key]
+operator fun ArgsParser.get(key: String, def: String): String = this.options[key] ?: def
